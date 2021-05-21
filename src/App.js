6d21b24cfa953334d348c996ae6601653c9fbe5e
OzB7505 <obal7505@richmondhillhs.org>
2021-05-21 17:21:54 +0000

    working on splash

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './App.css';
-
+import Navbar from './Components/Navbar';
+import Card from './Components/card.js'
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <Navbar/>
+      
+        <div className="container">
+        <ul className="cards">
+          <Card
+            title= 'Teacher card'
+            imageUrl='https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fforbestechcouncil%2Ffiles%2F2019%2F01%2Fcanva-photo-editor-8-7.jpg'
+            body='Teacher description'
+            />
+            <Card
+            title= 'Teacher card'
+            imageUrl='https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fforbestechcouncil%2Ffiles%2F2019%2F01%2Fcanva-photo-editor-8-7.jpg'
+            body='Teacher description'
+            />
+
+            
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './flex.css';
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <div><h1>RHHS SEP HOMEPAGE</h1></div>
+      <div class="flex-container">
+          <div>Teacher Page</div>
+          <div>page 2</div>
+          <div>page 3</div>
+          <div>page 4</div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
